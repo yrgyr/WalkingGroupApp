a3edfd47cbf70df6c1d9b4ff4d86882f1b101106
Mark Lee <mla26@sfu.ca>
2018-06-27 20:27:53 -0700

    Recreated Google Maps Activity using the Google Maps Activity template in Android Studio

--- app/src/main/java/sfucmpt276/walkinggroup/MainActivity.java
+++ app/src/main/java/sfucmpt276/walkinggroup/MainActivity.java
@@ -1,6 +1,7 @@
 package sfucmpt276.walkinggroup;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Test launching the MapActivity to make sure it runs correctly
+        startActivity(new Intent(this, MapsActivity.class));
     }
 
     // From video tutorial https://www.youtube.com/watch?v=urLA8z6-l3k&list=PLgCYzUzKIBE-vInwQhGSdnbyJ62nixHCt&index=3&t=0s
