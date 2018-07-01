80c6514be233e2eb615f15128e769fa466f46de7
Mark Lee <mla26@sfu.ca>
2018-06-30 20:52:01 -0700

    First implementation of Group class
    - Group class stores info about a walking group

--- app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/Group.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/dataobjects/Group.java
@@ -2,6 +2,9 @@ package ca.cmpt276.walkinggroup.dataobjects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Store information about the walking groups.
  *
@@ -10,4 +13,64 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Group extends IdItemBase{
+    private static Group singletonInstance;
+    private int id;
+    private String groupDescription;
+    private List<Double> routeLatArray = new ArrayList<>();
+    private List<Double> routeLngArray = new ArrayList<>();
+    private User leader;
+    private List<User> groupMembers;
+
+
+    public Group() { }
+
+    // Singleton support
+    public static Group getGroupSingletonInstance(){
+        if (singletonInstance == null){
+            singletonInstance = new Group();
+        }
+        return singletonInstance;
+    }
+
+    // Basic group data getters and setters
+
+    public void setGroupId(int id){ this.id = id; }
+    public int getGroupId() { return id; }
+
+    public void setGroupDescription(String groupDescription){this.groupDescription = groupDescription;}
+    public String getGroupDescription() { return groupDescription; }
+
+    public double getStartLat() {
+        return routeLatArray.get(0);
+    }
+
+    public void setStartLat(double lat){
+        this.routeLatArray.add(lat);
+    }
+
+    public double getStartLng() {
+        return routeLngArray.get(0);
+    }
+
+    public void setStartLng(double lng){
+        this.routeLngArray.add(lng);
+    }
+
+    public void setLeader(User leader) {
+        this.leader = leader;
+    }
+
+    public User getLeader() {
+        return leader;
+    }
+
+    public List<User> getGroupMembers() {
+        return groupMembers;
+    }
+
+    public void setGroupMembers(List<User> groupMembers) {
+        this.groupMembers = groupMembers;
+    }
+
+
 }
---
+++ app/src/test/java/ca/cmpt276/walkinggroup/dataobjects/GroupTest2.java
@@ -0,0 +1,75 @@
+package ca.cmpt276.walkinggroup.dataobjects;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GroupTest2 {
+
+    private Group TestGroup = new Group();
+
+
+    @Test
+    public void getGroupSingletonInstance() {
+        Group singleton = Group.getGroupSingletonInstance();
+        singleton.setGroupDescription("singleton");
+        Group group2 = new Group();
+        group2.setGroupDescription("another group");  // test if another regular group object can be instantiated
+        assertEquals("singleton", singleton.getGroupDescription());
+        assertEquals("another group", group2.getGroupDescription());
+    }
+
+    @Test
+    public void setGroupId() {
+        TestGroup.setGroupId(1);
+        assertEquals(1, TestGroup.getGroupId());
+    }
+
+    @Test
+    public void setGroupDescription() {
+        TestGroup.setGroupDescription("Test group 1");
+        assertEquals("Test group 1", TestGroup.getGroupDescription());
+    }
+
+    @Test
+    public void setStartLat() {
+        TestGroup.setStartLat(50.13);
+        assertEquals(50.13, TestGroup.getStartLat(),0.0001);
+    }
+
+    @Test
+    public void setStartLng() {
+        TestGroup.setStartLng(-30.12);
+        assertEquals(-30.12, TestGroup.getStartLng(),0.0001);
+    }
+
+    @Test
+    public void setLeader() {
+        User leader = new User();
+        leader.setName("group leader");
+        TestGroup.setLeader(leader);
+        User returnedLeader = TestGroup.getLeader();
+        assertEquals("group leader", returnedLeader.getName());
+    }
+
+    @Test
+    public void setGroupMembers() {
+        List<User> testUsers = new ArrayList<>();
+        for (int i = 0; i < 2; i++){
+            User user = new User();
+            user.setName("User " + i);
+            testUsers.add(user);
+        }
+
+        TestGroup.setGroupMembers(testUsers);
+        List<User> returnedUsers = TestGroup.getGroupMembers();
+
+        for (int i = 0; i < 2; i++){
+            User user = returnedUsers.get(i);
+            assertEquals("User " + i, user.getName());
+        }
+    }
+}
