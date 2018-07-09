7c5a67226f397d6cd54e49bfd7eb507f08bd8320
Mark Lee <mla26@sfu.ca>
2018-07-09 08:34:57 -0700

    Added 3 new empty activities
    - ParentsDashboard, EditContactInfo and SendMessage

---
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/EditContactInfo.java
@@ -0,0 +1,13 @@
+package ca.cmpt276.walkinggroup.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditContactInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact_info);
+    }
+}
