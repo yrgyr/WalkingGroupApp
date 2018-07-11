b8c4f5d76320d9a885c648dc00bdc69776e421ea
mirahman <mirahman@sfu.ca>
2018-07-10 20:19:14 -0700

    All update field working.

--- app/src/main/java/ca/cmpt276/walkinggroup/app/EditContactInfo.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/EditContactInfo.java
@@ -21,11 +21,26 @@ public class EditContactInfo extends AppCompatActivity {
 
     private EditText updateName;
     private EditText updateEmail;
+    private EditText updateContactNumber;
+    private EditText updateHomePhone;
+    private EditText updateAddress;
+    private EditText updateBirthYear;
+    private EditText updateBirthMonth;
+    private EditText updateTeacher;
+    private EditText updateGrade;
+    private EditText updateEmergency;
+
+
+
+
+
+
+
+
+
+
+
     private CurrentUserData userSingleton = CurrentUserData.getSingletonInstance();
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +50,6 @@ public class EditContactInfo extends AppCompatActivity {
         proxy = userSingleton.getCurrentProxy();
 
         user = userSingleton.getCurrentUser();
-
 
 
 
@@ -60,6 +74,32 @@ public class EditContactInfo extends AppCompatActivity {
         updateEmail=findViewById(R.id.updateEmail);
         updateEmail.setText(user.getEmail());
 
+        updateAddress=findViewById(R.id.updateAddress);
+        updateAddress.setText(user.getAddress());
+
+        updateContactNumber=findViewById(R.id.updatePhone);
+        updateContactNumber.setText(user.getCellPhone());
+
+        updateHomePhone=findViewById(R.id.updateHomePhone);
+        updateHomePhone.setText(user.getHomePhone());
+
+        updateGrade=findViewById(R.id.updateGrade);
+        updateGrade.setText(user.getGrade());
+
+        updateTeacher=findViewById(R.id.updateTeacherName);
+        updateTeacher.setText(user.getTeacherName());
+
+        updateEmergency=findViewById(R.id.updateEmergencyContact);
+        updateEmergency.setText(user.getEmergencyContactInfo());
+
+        /*updateBirthYear=findViewById(R.id.updateYear);
+        updateBirthYear.setText(user.getBirthYear());
+
+        updateBirthMonth=findViewById(R.id.updateMonth);
+        updateBirthMonth.setText(user.getBirthMonth());*/
+
+
+
     }
 
     //Set the new info in the User class
@@ -71,10 +111,40 @@ public class EditContactInfo extends AppCompatActivity {
         String newEmail=updateEmail.getText().toString();
         user.setEmail(newEmail);
 
+        String newAddress=updateAddress.getText().toString();
+        user.setAddress(newAddress);
+
+        String newCellPhone=updateContactNumber.getText().toString();
+        user.setCellPhone(newCellPhone);
+
+        String newHomePhone=updateHomePhone.getText().toString();
+        user.setHomePhone(newHomePhone);
+
+        String newGrade=updateGrade.getText().toString();
+        user.setGrade(newGrade);
+
+        String newTeacher=updateTeacher.getText().toString();
+        user.setTeacherName(newTeacher);
+
+        String newEmergency=updateEmergency.getText().toString();
+        user.setEmergencyContactInfo(newEmergency);
+
+        /*String month=updateBirthMonth.getText().toString();
+        int newMonth=Integer.parseInt(month);
+        user.setBirthMonth(newMonth);
+
+        String year=updateBirthYear.getText().toString();
+        int newYear=Integer.parseInt(year);
+        user.setBirthYear(newYear);*/
+
+
+
+
+
 
 
     }
-    //D
+
     private void setupValidateButton() {
         Button doneBtn=(Button) findViewById(R.id.doneBtn);
         doneBtn.setOnClickListener(new View.OnClickListener() {
