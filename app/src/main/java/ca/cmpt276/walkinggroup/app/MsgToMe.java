5c60de974df97787b6c08a92538dcc8e81fd5589
guoyiran <yga58@sfu.ca>
2018-07-15 17:16:09 -0700

    when open the mail box , automatically set message text style from bold to default if the message has already been read

--- app/src/main/java/ca/cmpt276/walkinggroup/app/MsgToMe.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/MsgToMe.java
@@ -1,15 +1,19 @@
 package ca.cmpt276.walkinggroup.app;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import ca.cmpt276.walkinggroup.dataobjects.CurrentUserData;
@@ -18,20 +22,23 @@ import ca.cmpt276.walkinggroup.dataobjects.User;
 import ca.cmpt276.walkinggroup.proxy.ProxyBuilder;
 import ca.cmpt276.walkinggroup.proxy.WGServerProxy;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Path;
 
 public class MsgToMe extends AppCompatActivity {
 
-    WGServerProxy proxy = CurrentUserData.getSingletonInstance().getCurrentProxy();
-    User currentUser = CurrentUserData.getSingletonInstance().getCurrentUser();
+    private WGServerProxy proxy = CurrentUserData.getSingletonInstance().getCurrentProxy();
+    private User currentUser = CurrentUserData.getSingletonInstance().getCurrentUser();
 
-    List<Message> all_msgs;
+    private List<Message> all_Msgs;
+
+    private ArrayList<String> messages;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_msg_to_me);
 
         displayMsgs();
-
         setupOnItemClick();
 
 
@@ -43,16 +50,17 @@ public class MsgToMe extends AppCompatActivity {
         messages.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Message msg = all_msgs.get(position);
 
                 TextView tv = (TextView) view.findViewById(R.id.msg_item);
                 tv.setTypeface(Typeface.DEFAULT);
+
+//                Long msgID = all_Msgs.get(position).getId();
+
+
             }
         });
 
     }
-
-
     private void displayMsgs() {
 
         Call<List<Message>> caller = proxy.getMessages(currentUser.getId());
@@ -61,21 +69,64 @@ public class MsgToMe extends AppCompatActivity {
 
     private void responseAllMsg(List<Message> returnedMsgs) {
 
-        all_msgs = returnedMsgs;
-        ArrayList<String> ALL_msgs = new ArrayList<String>();
-        for(int i =0; i < returnedMsgs.size();i++){
+        all_Msgs = returnedMsgs;
 
-
-            Message THIS_MSG = returnedMsgs.get(i);
-            String text = THIS_MSG.getText();
-
-            ALL_msgs.add(text);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.users_list,ALL_msgs);
         ListView msg_list = (ListView) findViewById(R.id.msgListView);
+        ArrayAdapter<Message> adapter = new ArrayAdapter<Message>(this,R.layout.users_list,returnedMsgs){
 
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                View itemView = convertView;
+                if(itemView == null){
+                    itemView = getLayoutInflater().inflate(R.layout.users_list,parent,false);
+                }
+
+                Message currentMsg = returnedMsgs.get(position);
+
+
+
+
+                Date timestamp = currentMsg.getTimestamp();
+                String display_msg = currentMsg.getText() + " \n" + timestamp ;
+
+                TextView tv = (TextView) itemView.findViewById(R.id.msg_item);
+                tv.setText(display_msg);
+
+                if(currentMsg.isRead()){
+                    tv.setTypeface(Typeface.DEFAULT);
+                }
+                return itemView;
+            }
+
+        };
         msg_list.setAdapter(adapter);
-    }
 
+
+//        for(int i =0; i < returnedMsgs.size();i++){
+//            Message THIS_MSG = returnedMsgs.get(i);
+//            if(THIS_MSG.isRead() == true){
+//                View v = msg_list.getChildAt(i - msg_list.getFirstVisiblePosition());
+//                if(v == null)
+//                    return;
+//                TextView tv = (TextView) v.findViewById(R.id.msg_item);
+//                tv.setTypeface(Typeface.DEFAULT);
+//
+//            }
+//
+//        }
+
+
+    }
+//    private class Myadapter extends ArrayAdapter<String>{
+//
+//        public Myadapter(){
+//            super(MsgToMe.this,R.layout.users_list,messages);
+//
+//        }
+//
+
+//    }
 }
