4fecabec311803c00ad3a7a62349a21ba97bf95c
Mark Lee <mla26@sfu.ca>
2018-07-26 15:14:52 -0700

    Able to correctly update points on the server when a walk is completed

--- app/src/main/java/ca/cmpt276/walkinggroup/app/MapsActivity.java
+++ app/src/main/java/ca/cmpt276/walkinggroup/app/MapsActivity.java
@@ -73,13 +73,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     private boolean uploadingLocation = userSingleton.getUploadingLocation();
-    private final int GPS_UPLOAD_INTERVAL_IN_MILLISEC = 30000;
+    private final int GPS_UPLOAD_INTERVAL_IN_MILLISEC = 5000;  // Todo: 30000
     private final int GPS_UPLOAD_MIN_DIST_IN_METERS = 0;
     private GpsLocation currentGpsLocation = new GpsLocation();
 
     // Countdown timer when user reaches school
-    private final int GPS_COUNTDOWN_INTERVAL_IN_MILLISEC = 600000;
-    private final int SCHOOL_RADIUS_IN_METERS = 200;
+    private final int GPS_COUNTDOWN_INTERVAL_IN_MILLISEC = 10000;  // Todo: 600000
+    private final int SCHOOL_RADIUS_IN_METERS = 1000;  // Todo: 200
     private CountDownTimer DestReachedCountDown = userSingleton.getDestReachedCountDown();
     private boolean DestReachedCountDownRunning = userSingleton.isDestReachedCountDownRunning();
 
@@ -443,6 +443,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         currentUser.setCurrentPoints(currentPoints);
         currentUser.setTotalPointsEarned(totalPoints);
 
-        // Todo: call server to editUser
+        Call<User> caller = proxy.editUser(currentUser, currentUser.getId());
+        ProxyBuilder.callProxy(MapsActivity.this, caller, updatedUser -> responseEditUser(updatedUser));
+    }
+
+    private void responseEditUser(User user){
+        Toast.makeText(MapsActivity.this, "Current points: " + user.getCurrentPoints() + ", total points: " + user.getTotalPointsEarned(), Toast.LENGTH_LONG).show();
     }
 }
